e1ca71c83d47d18aed3556946723b8854b2be94d
woo.nyung <woo.nyung0520@gmail.com>
2026-09-10 16:53:32 +0900

    JsonFile 추가, JsonFile 읽기

--- MyDogamDesktop/Forms/MainForm.cs
+++ MyDogamDesktop/Forms/MainForm.cs
@@ -1,4 +1,5 @@
 using MyDogamDesktop.Services;
+using MyDogamDesktop.Models;
 
 namespace MyDogamDesktop
 {
@@ -7,6 +8,11 @@ namespace MyDogamDesktop
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LoadCollections();
         }
 
         private async void btnUpload_Click(object sender, EventArgs e)
@@ -32,15 +38,31 @@ namespace MyDogamDesktop
             }
         }
 
+        private List<Collection> _collections = new List<Collection>();
         private async void LoadCollections()
         {
             var service = new CollectionService();
-            var collections = await service.GetAllCollectionsAsync();
+            _collections = await service.GetAllCollectionsAsync();
 
             lstCollections.Items.Clear();
-            foreach (var c in collections)
+            foreach (var c in _collections)
             {
                 lstCollections.Items.Add($"{c.Name} ({c.TotalItems}개)");
+            }
+        }
+
+        private async void lstCollections_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstCollections.SelectedIndex == -1) return;
+
+            var selected = _collections[lstCollections.SelectedIndex];
+            var service = new CollectionService();
+            var items = await service.GetItemsByCollectionIdAsync(selected.Id);
+
+            lstItems.Items.Clear();
+            foreach(var item in items)
+            {
+                lstItems.Items.Add($"{item.ItemId} - {item.Name} (수량: {item.Count})");
             }
         }
     }
